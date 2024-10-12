338f4a7f5735c5c26b5e002b9b6233d63acffe70
Niyastheen.N <niyastheenofficial@gmail.com>
2024-10-12 10:52:37 +0530

    uodates files

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
         <div className='bg-[#f8f8f5]'>
             <div className='container mx-auto text-[#11302b] text-center py-12 px-6  '>
                 <h1 className='font-serif text-[#11302b] text-4xl pb-8'>OUR STORY</h1>
-                <span className='text-sm md:text-xl '>
+                <span className='font-serif text-sm md:text-lg '>
                     Fervoro was born out of a passion for the refined lifestyle that polo represents.
                     We saw an opportunity to blend the classic elements of polo with modern design,
                     creating a brand that celebrates both heritage and contemporary fashion.
