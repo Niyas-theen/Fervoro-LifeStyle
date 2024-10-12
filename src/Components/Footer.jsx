0dd472fef9eab87f68c934b6e3f451d1fda92b97
Niyastheen.N <niyastheenofficial@gmail.com>
2024-10-12 13:32:22 +0530

    footer new

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -31,6 +31,9 @@ const Footer = () => {
                 </div>
                 <hr className='border-[#11302b]' />
             </div>
+            <div className="text-center font-serif text-[#11302b] text-sm px-5 py-3">
+                © Copyright 2024 All Rights Reserved Designed & Developed by Zeal Designing Studio
+            </div>
         </div>
     )
 }
